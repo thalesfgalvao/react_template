65a886678b86092b64fc9067fabbb0353dd6e962
Thales Marques <thalesf.galvao@gmail.com>
2026-06-30 13:00:28 -0300

    Create theme provider in App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import React from "react";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./styles/theme";
 
-createRoot(document.getElementById('root')!).render(
- <React.StrictMode>
-    <GlobalStyle />
-    <App />
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
